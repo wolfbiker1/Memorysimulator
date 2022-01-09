fa4e0cc4d2981b872f86abf1e360aa57dedf9bab
Benjamin Wilhelm <be631wil@htwg-konstanz.de>
2022-01-09 13:56:56 +0100

    feat: simulator

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 extern crate mem;
 fn main() {
-    mem::init_memory();
-    mem::dump_memory();
+    mem::init(0x2000_0000, 0x2000_0FFF);
+
+    mem::write(0x20000000, 0xABCD0101);
+    mem::write(0x20000000, mem::read(0x2000_0000) & !(0xFFFF));
+    println(mem::read(0x2000_0000));
+}
+
+pub fn println(value: u32) {
+    println!("0x{:08x}", value);
 }
